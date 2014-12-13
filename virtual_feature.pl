9985dcbe1d68a11eb811f4b9849d951fed5c862b
Jamie Cameron <jcameron@webmin.com>
2014-12-13 15:58:02 -0800

    Use safe API for writing backup files

--- virtual_feature.pl
+++ virtual_feature.pl
@@ -293,7 +293,7 @@ local ($d, $file, $opts) = @_;
 &$virtual_server::first_print($text{'feat_backup'});
 local $cfile = "$config{'vsftpd_dir'}/vsftpd.$d->{'dom'}.conf";
 if (-r $cfile) {
-	&copy_source_dest($cfile, $file);
+	&virtual_server::copy_write_as_domain_user($d, $cfile, $file);
 	&$virtual_server::second_print($virtual_server::text{'setup_done'});
 	return 1;
 	}
